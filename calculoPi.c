4b4903dde5df221d560b8c636eba35f6d667620f
Léo Zambone <138814524+LeoZamboneNeto@users.noreply.github.com>
2023-08-27 10:18:54 -0300

    Create calculoPi.c

---
+++ calculoPi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int termos;
+    double pi = 0; //Declaração do início da variável para precisão de cálculo. 
+    int i;
+    
+    printf("Escolha o número de termos para o cálculo de pi, quanto mais termos usados, mais próximo do número real de pi estará: ");
+    scanf("%i", &termos);
+
+    //Base cálculo de Pi em base ao Gottfried Leibniz
+    for (i = 0; i < termos; i++) {
+        if (i % 2 == 0) {
+            pi += 1.0 / (2 * i + 1);
+        } else {
+            pi -= 1.0 / (2 * i + 1);
+        }
+    }
+
+    pi *= 4; // Multiplicar pelo fator 4 para obter π
+
+    printf("Valor de pi calculado com %i termos: %.20lf\n", termos, pi);
+
+    return 0;
+}
